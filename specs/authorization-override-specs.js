c9d9cbb3140292209ee6de74758a9e3406c78b22
Derick Bailey <derickbailey@gmail.com>
2014-07-07 14:04:28 -0500

    placeholder specs for authorization override methods

---
+++ specs/authorization-override-specs.js
@@ -0,0 +1,27 @@
+describe("authorization overrides", function(){
+
+  describe("when an activity is explicitly denied", function(){
+
+    it("should not allow the request", function(){
+      throw new Error("not implemented");
+    });
+
+  });
+
+  describe("when an activity is explicitly allowed", function(){
+
+    it("should allow the request", function(){
+      throw new Error("not implemented");
+    });
+
+  });
+
+  describe("when an activity is explicitly denied and allowed", function(){
+
+    it("should not allow the request", function(){
+      throw new Error("not implemented");
+    });
+
+  });
+
+});
