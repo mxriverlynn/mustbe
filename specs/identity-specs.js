77c1cc97a38dbe5f140cc8315fc001162eab9cac
Derick Bailey <derickbailey@gmail.com>
2015-12-17 12:09:11 -0600

    uncommenting a spec, and removing some that i don't know why they existed

--- specs/identity-specs.js
+++ specs/identity-specs.js
@@ -3,27 +3,6 @@ var helpers = require("./helpers");
 
 describe("identity", function(){
 
-  describe("when getting identity and not providing an identity type to get", function(){
-    var id;
-
-    beforeEach(function(){
-      var mustBe = new MustBe();
-
-      mustBe.configure(function(config){
-        config.userIdentity(function(id){
-          id.isAuthenticated(function(){});
-        });
-      });
-
-      id = mustBe.getIdentity();
-    });
-
-    it("should return the default identity", function(){
-      expect(id).not.toBe(null);
-    });
-
-  });
-
   describe("when getting a custom identity by type", function(){
     var Identity, IdentityType, id;
 
@@ -45,14 +24,6 @@ describe("identity", function(){
     it("should return the custom identity", function(){
       expect(id.type).toBe(IdentityType);
     });
-  });
-
-  xdescribe("when userIdentity is configured with an isAuthenticated method", function(){
-
-    it("should return whether or not the user is authenticated", function(){
-      throw new Error("not implemented");
-    });
-
   });
 
 });
